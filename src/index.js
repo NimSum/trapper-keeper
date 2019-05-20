32074ae4a1c18a5f5e38a07249d9d9664d7107ac
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-20 15:44:48 -0600

    Rootreducer setup Co-authored-by: Bridgett Coyle <bacoyle0409@gmail.com> Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './containers/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
