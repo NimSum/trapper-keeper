c1482d705e635965e2efbca70a2f134c967fa21c
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-22 15:24:12 -0600

    Create class component for listitem Co-authored-by: Adam Niedzwiecki <AdamN8142@gmail.com>

---
+++ src/containers/ListItem/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ListItem extends Component {
+  render() {
+    return (
+      <ul>
+        
+      </ul>
+    )
+  }
+}
+
+export default ListItem
