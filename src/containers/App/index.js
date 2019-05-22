bfce76e908d6f14873eee0598053fee720176e54
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-22 14:47:16 -0600

    Create add notes action for redux Create Co-authored-by: Adam Niedzwiecki <AdamN8142@gmail.com>

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,23 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addNotes } from '../../actions/'
+export class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  render() {
+    return (
+      <div>
+        <h1>Trapper Keeper</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,5 +1,7 @@
 export const notesReducer = (state = [], action) => {
   switch(action.type) {
+    case 'ADD_NOTES':
+      return [...action.notes]
     default: 
     return state;
   }
