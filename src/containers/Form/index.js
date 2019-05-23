99f4563e57ada613ef83a80403224f1aacfd5642
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-22 22:54:17 -0600

    Create dynamic handlechange for note title input and state

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -5,24 +5,32 @@ export class Form extends Component {
   constructor() {
     super();
     this.state = {
+      title: '',
       listItems: [
-        {body: 'asdfasdf'}
+        { body: 'asdfasdf',
+          id: 1 }
       ]
-    }
+    };
   }
 
-  
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value })
+  }
 
   render() {
     return (
       <div>
         <div>
-          <input placeholder='Title' type= 'text'/> 
+          <input 
+            placeholder='Title' 
+            type='text'
+            name='title'
+            onChange={ this.handleChange } /> 
         </div>
         <div>
-         {this.state.listItems.map(item => (
-           <input value={item.body} />
-         ))}
+          {this.state.listItems.map(item => (
+            < ListItem item={ item } />
+          ))}
         </div>
       </div>
     )
