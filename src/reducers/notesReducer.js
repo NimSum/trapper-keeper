0b055d97201377177146a7fdf58224df4ba36cbf
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-23 12:29:48 -0600

    Generate new notes on submit

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,9 +1,9 @@
 export const notesReducer = (state = [], action) => {
   switch(action.type) {
     case 'ADD_NOTES':
-      return [...action.notes]
+      return [...action.notes ]
     case 'ADD_NOTE':
-      return [...action.note, ...state ]
+      return [ {...action.note }, ...state ]
     default: 
     return state;
   }
