ac70d81610aee3acb0dca824931306942558c8c2
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-23 11:31:06 -0600

    Update notesreducer to ADD_NOTE action Co-authored-by: Bridgett Coyle <bacoyle0409@gmail.com>

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -2,6 +2,8 @@ export const notesReducer = (state = [], action) => {
   switch(action.type) {
     case 'ADD_NOTES':
       return [...action.notes]
+    case 'ADD_NOTE':
+      return [...action.note, ...state ]
     default: 
     return state;
   }
