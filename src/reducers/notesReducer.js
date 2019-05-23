926e68d84dc4e6dabd680c88acaec5f59febf367
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-23 13:17:43 -0600

    Create update list item notesreducer case statement
    Co-authored-by: Bridgett Coyle <bacoyle0409@gmail.com>
    Co-authored-by: Adam Niedzwiecki <AdamN8142@gmail.com>
    Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -4,6 +4,17 @@ export const notesReducer = (state = [], action) => {
       return [...action.notes ]
     case 'ADD_NOTE':
       return [ {...action.note }, ...state ]
+    case 'UPDATE_LIST_ITEM':
+      const updated = [...state].map(note => {
+        note.listItems.map(listItem => {
+          if (listItem.id === action.listItem.id) {
+            listItem = action.listItem
+          }
+          return listItem;
+        })
+        return note;
+      })
+      return updated;
     default: 
     return state;
   }
