05e3ff90809f9ebf27e3cd2ff7d8105e5e64c047
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-25 19:21:08 -0600

    Add passing test for actions ADD_NOTES
    Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>

---
+++ src/actions/actions.test.js
@@ -0,0 +1,54 @@
+import * as actions from '../actions';
+
+describe('actions', () => {
+    let mockNotes
+
+    beforeEach(() => {
+        mockNotes = [
+            {
+                "title": "randomnote",
+                "id": "1",
+                "listItems": [
+                    {
+                        "id": "1",
+                        "body": "asdf",
+                        "completed": false
+                    }
+                ]
+            },
+            {
+                "title": "randomnoteTWO",
+                "id": "2",
+                "listItems": [
+                    {
+                        "id": "2",
+                        "body": "asdf",
+                        "completed": false
+                    }
+                ]
+            },
+            {
+                "id": "21f67edd-ec5a-4ab4-8b9e-18fa1326a70e",
+                "title": "Angry Man",
+                "listItems": [
+                    {
+                        "id": "92f3600b-00e5-4551-82c3-a1c7b409d9d2",
+                        "body": "Yelling in a coffee shop",
+                        "completed": false
+                    }
+                ]
+            }
+        ]
+
+    });
+
+    it("should create an action object with a type of ADD_NOTES",() => {
+        const expected = "ADD_NOTES";
+        const results = actions.addNotes(mockNotes)
+        expect(results.type).toBe(expected);
+    })
+
+
+
+
+})
