f1ed8f86b803feb1d6932c2ddcf635d586fd698d
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-26 17:39:58 -0600

    Add functionality for moving completed items to bottom of notecard
    Co-authored-by: Adam Niedzwiecki <AdamN8142@gmail.com>
    Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>

--- src/containers/NoteCard/index.js
+++ src/containers/NoteCard/index.js
@@ -56,18 +56,23 @@ export class NoteCard extends Component {
   }
 
   render() {
-    const listItems = this.props.note.listItems.map(item => (
-      < ListItem 
-        updateListItems={ this.updateListItems } 
-        item={ item } />
-    ))
+    const completedListItems = this.props.note.listItems.filter(item => {
+      return item.completed }).map(filteredItem => 
+      (<ListItem updateListItems={ this.updateListItems } item={ filteredItem } />)
+    )
+
+    const uncompletedListItems = this.props.note.listItems.filter(item => {
+      return !item.completed}).map(filteredItem => 
+      (<ListItem updateListItems={ this.updateListItems } item={ filteredItem } />)
+    )
 
     return (
       <NavLink exact to={`/notes/${this.props.note.id}`} style={{ textDecoration: 'none'}} activeClassName='active'>
         <article className='note-card'>
           <h3>{this.props.note.title}</h3>
           <ul className='note-items'>
-            { listItems }
+            { uncompletedListItems }
+            { <hr /> && completedListItems }
           </ul>
           <button className="delete-card" onClick={this.deleteNote}>X</button>
         </article>
