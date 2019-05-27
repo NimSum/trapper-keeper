f4120dae3aca3a9209dd9d4487adaa3e04bfdf4f
Adam Niedzwiecki <adamn8142@gmail.com>
2019-05-27 12:36:31 -0600

    Add inital CardContainer test setup
    Co-authored-by: Bridgett Coyle <bacoyle0409@gmail.com>
    Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>

---
+++ src/components/CardContainer/CardContainer.test.js
@@ -0,0 +1,45 @@
+import { shallow } from 'enzyme';
+import { CardContainer } from './index.js';
+
+describe('CardContainer', () => {
+  let wrapper;
+  let mockNotes;
+  beforeEach(()=> {
+    mockNotes = [
+      {
+        title: "randomnote",
+        id: "1",
+        listItems: [
+          {
+            id: "1",
+            body: "asdf",
+            completed: false
+          }
+        ]
+      },
+      {
+        title: "randomnoteTWO",
+        id: "2",
+        listItems: [
+          {
+            id: "2",
+            body: "asdf",
+            completed: false
+          }
+        ]
+      },
+      {
+        id: "21f67edd-ec5a-4ab4-8b9e-18fa1326a70e",
+        title: "Angry Man",
+        listItems: [
+          {
+            id: "92f3600b-00e5-4551-82c3-a1c7b409d9d2",
+            body: "Yelling in a coffee shop",
+            completed: false
+          }
+        ]
+      }
+    ];
+    wrapper = enzyme.shallow(<NoteCard note={mockNotes} key={mockNotes.id})
+  })
+})
