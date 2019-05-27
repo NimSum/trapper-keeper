bcda02e3c7f98f3d7616ab542c1818ef36e59946
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-27 13:03:59 -0600

    Test initial state

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -6,5 +6,22 @@ import { putNote } from '../../utils/apiFetches/putNote';
 import  uuidv4 from 'uuid/v4';
 
 describe('Form Container', () => {
-  
+  const initialState = {
+    title: '',
+    listItemText: '',
+    listItems: [],
+    editing: false,
+    id: '',
+    redirect: false
+  }
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow( <Form /> )
+  })
+
+  it('should have initial state', () => {
+    expect(wrapper.state()).toEqual(initialState);
+  })
+
 })
