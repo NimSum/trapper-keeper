5ae9afeb378aff85d62d455883d774f630816aa0
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-27 12:56:02 -0600

    Setup imports and secribe block for testing

---
+++ src/containers/Form/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Form, mapDispatchToProps } from './index';
+import { shallow } from 'enzyme';
+import { addNewNote } from '../../thunks/addNewNote';
+import { putNote } from '../../utils/apiFetches/putNote';
+import  uuidv4 from 'uuid/v4';
+
+describe('Form Container', () => {
+  
+})
